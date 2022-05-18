60f656df881f2c2cd61f80368d3b6bece004978e
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2022-05-18 14:30:09 +0700

    Update #3

--- src/queue.c
+++ src/queue.c
@@ -20,28 +20,20 @@ struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	if(q->size != 0){
-		struct pcb_t* search=q->proc[0];
-		int search_priority = search->priority;
-		int search_index = 0;
-		for(int i=1; i<q->size; ++i){
-			if(q->proc[i]->priority > search_priority){
-				search = q->proc[i];
-				search_priority = q->proc[i]->priority;
-				search_index = i;
-			}
+	
+	if(q->size == 0) return NULL;
+	
+	int search = 0;
+
+	for(int i = 1; i < q->size; ++i){
+		if(q->proc[i]->priority < q->proc[search]->priority){
+			search = i;
 		}
-		// move up index if search_index<q->size-1
-		// else just size = size -1
-		for(int i = search_index ; i < q->size-1; ++i ){
-			q->proc[i] = q->proc[i+1];
-		}
-		
-		//then 
-		q->proc[q->size-1]=NULL;
-		q->size -= 1;
-		return search;
 	}
-	return NULL;
+
+	struct pcb_t* _search = q->proc[search];
+	q->proc[search] = q->proc[q->size - 1];
+	q->size -= 1;
+	return _search;
 }
 
